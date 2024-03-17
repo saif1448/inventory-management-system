758f548891cf4e6abe0cb2f793560a7c1f179b36
Saif Mahmud Parvez <mahmudalsaif@gmail.com>
2024-03-17 16:30:20 +0600

    added repositories and created tables in db

--- src/main/java/com/example/inventorymanagement/models/User.java
+++ src/main/java/com/example/inventorymanagement/models/User.java
@@ -1,5 +1,26 @@
 package com.example.inventorymanagement.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_db")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String userAddress;
+    private String userDesignagtion;
+    private String userPassword;
 }
